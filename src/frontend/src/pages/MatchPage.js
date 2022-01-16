682f7de1526d60a1210a2d247d162bebd3e072f8
Sachin Thirumala <sachin.thirumala@walmart.com>
2022-01-16 14:40:39 +0530

    Add match page in react app

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,12 +5,27 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+    useEffect(
+        () => {
+         const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
 
+         };
+         fetchMatches();
+
+        }, []
+    );
     
     return (
       <div className="MatchPage">
         <h3>Match Page</h3>
-
+        {
+            matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
+        }
       </div>
     );
 }
