c351e84d84c6d99e8d6034fd48207103ab542a20
Sachin Thirumala <sachin.thirumala@walmart.com>
2022-01-22 14:51:35 +0530

    Match page year selector and home page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/teams/:teamName" element={<TeamPage/>}>
         </Route>
         <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>}>
+        </Route>
+        <Route path="/team" element={<HomePage/>}>
         </Route>
         </Routes>
       </BrowserRouter>
